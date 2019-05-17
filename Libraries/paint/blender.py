d09a200ec513bc82afb145bb2fa7eac09c71925e
luboslenco <lubos.lenco@gmail.com>
2019-05-17 14:06:46 +0200

    Emission define

--- Libraries/paint/blender.py
+++ Libraries/paint/blender.py
@@ -11,11 +11,11 @@ def on_make_renderpath():
     arm.assets.add(arm.utils.get_sdk_path() + '/armory/Assets/noise256.png')
     arm.assets.add_embedded_data('noise256.png')
     bpy.data.worlds['Arm'].world_defs += '_CGrainStatic'
-
+    bpy.data.worlds['Arm'].world_defs += '_Emission'
     arm.assets.add_khafile_param('--macro include("arm.brushnode")')
+    # arm.assets.add_khafile_param('--macro include("armory.logicnode")')
     arm.assets.add_khafile_def('arm_appwh')
     arm.assets.add_khafile_def('arm_skip_envmap')
-    # arm.assets.add_khafile_param('--macro include("armory.logicnode")')
 
 def register():
     arm.make_renderpath.callback = on_make_renderpath
