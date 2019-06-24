01ee7d3af45ae87f90a753bcc90d85bc543186e6
luboslenco <lubos.lenco@gmail.com>
2019-06-24 10:46:45 +0200

    Begin operator

--- Sources/arm/Operator.hx
+++ Sources/arm/Operator.hx
@@ -2,5 +2,15 @@ package arm;
 
 class Operator {
 
-	
+	public static var run = new Map<String, Dynamic>();
+	public static var keymap = new Map<String, String>();
+
+	public static function register(name:String, key:String, call:Dynamic) {
+		run[name] = call;
+		keymap[key] = name;
+	}
+
+	public static function update() {
+
+	}
 }
