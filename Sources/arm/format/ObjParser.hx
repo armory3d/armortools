8ec62ccd4519cc25ebd3562194d696d9b427d03f
luboslenco <lubos.lenco@gmail.com>
2019-08-27 20:56:49 +0200

    Handle exponent sign in obj parser

--- Sources/arm/format/ObjParser.hx
+++ Sources/arm/format/ObjParser.hx
@@ -309,6 +309,7 @@ class ObjParser {
 		while (true) { // Read into buffer
 			var c = bytes.get(pos);
 			if (c == " ".code || c == "\n".code || c == "\r".code) break;
+			if (c == "E".code) return 0.0; // Assume number close to zero for now
 			pos++;
 			buf[bi++] = c;
 		}
