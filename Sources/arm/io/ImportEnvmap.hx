699cbeb786d313425d3561d6820a6f365bccbb76
luboslenco <lubos.lenco@gmail.com>
2019-09-15 20:52:34 +0200

    Fix envmap import

--- Sources/arm/io/ImportEnvmap.hx
+++ Sources/arm/io/ImportEnvmap.hx
@@ -12,8 +12,7 @@ using StringTools;
 class ImportEnvmap {
 
 	public static function run(path:String, image:Image) {
-		var dataPath = Data.dataPath;
-		var p = Krom.getFilesLocation() + '/' + dataPath;
+		var p = Krom.getFilesLocation() + '/' + Data.dataPath;
 		#if krom_windows
 		var cmft = p + "/cmft.exe";
 		#elseif krom_linux
@@ -22,15 +21,17 @@ class ImportEnvmap {
 		var cmft = p + "/cmft-osx";
 		#end
 
-		var cmd = '';
-		var tmp = Krom.getFilesLocation() + '/' + dataPath;
+		var tmp = Krom.getFilesLocation() + '/' + Data.dataPath;
 
 		// Irr
-		cmd = cmft;
+		var cmd = cmft;
 		cmd += ' --input "' + path + '"';
 		cmd += ' --filter shcoeffs';
 		cmd += ' --outputNum 1';
 		cmd += ' --output0 "' + tmp + 'tmp_irr"';
+		#if krom_windows
+		cmd = cmd.replace('/', '\\');
+		#end
 		Krom.sysCommand(cmd);
 
 		// Rad
@@ -58,10 +59,13 @@ class ImportEnvmap {
 		cmd += ' --outputNum 1';
 		cmd += ' --output0 "' + tmp + 'tmp_rad"';
 		cmd += ' --output0params hdr,rgbe,latlong';
+		#if krom_windows
+		cmd = cmd.replace('/', '\\');
+		#end
 		Krom.sysCommand(cmd);
 
 		// Load irr
-		Data.getBlob(tmp + "tmp_irr.c", function(blob:Blob) {
+		Data.getBlob("tmp_irr.c", function(blob:Blob) {
 			var lines = blob.toString().split("\n");
 			var band0 = lines[5];
 			var band1 = lines[6];
@@ -73,43 +77,51 @@ class ImportEnvmap {
 			band = band.replace("{", "");
 			band = band.replace("}", "");
 			var ar = band.split(",");
-			var buf = new Float32Array(27);
-			for (i in 0...ar.length) buf[i] = Std.parseFloat(ar[i]);
+			var buf = new Float32Array(28); // Align to mult of 4 - 27->28
+			for (i in 0...27) buf[i] = Std.parseFloat(ar[i]);
 			Scene.active.world.probe.irradiance = buf;
 			Context.ddirty = 2;
-			Data.deleteBlob(tmp + "tmp_irr.c");
+			Data.deleteBlob("tmp_irr.c");
 		});
 
 		// World envmap
+		Scene.active.world.probe.raw.strength = 1.0;
 		Scene.active.world.envmap = image;
 		UITrait.inst.savedEnvmap = image;
 		UITrait.inst.showEnvmapHandle.selected = UITrait.inst.showEnvmap = true;
 
-		// Load mips
-		var mips = Std.int(image.width / 1024);
-		var mipsCount = 6 + mips;
-		var mipsLoaded = 0;
-		var mips:Array<Image> = [];
-		while (mips.length < mipsCount + 2) mips.push(null);
-		var mw = Std.int(image.width / 2);
-		var mh = Std.int(image.width / 4);
-		for (i in 0...mipsCount) {
-			Data.getImage(tmp + "tmp_rad_" + i + "_" + mw + "x" + mh + ".hdr", function(mip:Image) {
-				mips[i] = mip;
-				mipsLoaded++;
-				if (mipsLoaded == mipsCount) {
-					// 2x1 and 1x1 mips
-					mips[mipsCount] = Image.create(2, 1, TextureFormat.RGBA128);
-					mips[mipsCount + 1] = Image.create(1, 1, TextureFormat.RGBA128);
-					// Set radiance
-					image.setMipmaps(mips);
-					Scene.active.world.probe.radiance = image;
-					Scene.active.world.probe.radianceMipmaps = mips;
-					Context.ddirty = 2;
-				}
-			}, true); // Readable
-			mw = Std.int(mw / 2);
-			mh = Std.int(mh / 2);
-		}
+		// Load envmap clone and set mipmaps
+		Data.cachedImages.remove(path);
+		@:privateAccess Data.loadingImages.remove(path);
+		Data.getImage(path, function(image:kha.Image) {
+
+			// Load mips
+			var mipsCount = 6 + Std.int(image.width / 1024);
+			var mipsLoaded = 0;
+			var mips:Array<Image> = [];
+			while (mips.length < mipsCount + 2) mips.push(null);
+			var mw = Std.int(image.width / 2);
+			var mh = Std.int(image.width / 4);
+			for (i in 0...mipsCount) {
+				Data.getImage("tmp_rad_" + i + "_" + mw + "x" + mh + ".hdr", function(mip:Image) {
+					mips[i] = mip;
+					mipsLoaded++;
+					if (mipsLoaded == mipsCount) {
+						// 2x1 and 1x1 mips
+						var b2x1 = haxe.io.Bytes.alloc(2 * 1 * 4 * 4); // w * h * channels * format_size
+						var b1x1 = haxe.io.Bytes.alloc(1 * 1 * 4 * 4);
+						mips[mipsCount] = Image.fromBytes(b2x1, 2, 1, TextureFormat.RGBA128, kha.graphics4.Usage.DynamicUsage);
+						mips[mipsCount + 1] = Image.fromBytes(b1x1, 1, 1, TextureFormat.RGBA128, kha.graphics4.Usage.DynamicUsage);
+						// Set radiance
+						image.setMipmaps(mips);
+						Scene.active.world.probe.radiance = image;
+						Scene.active.world.probe.radianceMipmaps = mips;
+						Context.ddirty = 2;
+					}
+				}, true); // Readable
+				mw = Std.int(mw / 2);
+				mh = Std.int(mh / 2);
+			}
+		});
 	}
 }
