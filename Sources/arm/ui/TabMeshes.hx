7da8a1bbd2bfbf2513a83a438f0dd9da60b5336d
luboslenco <lubos.lenco@gmail.com>
2019-11-07 18:57:43 +0100

    Meshes - Tools menu

--- Sources/arm/ui/TabMeshes.hx
+++ Sources/arm/ui/TabMeshes.hx
@@ -1,44 +1,46 @@
 package arm.ui;
 
+import zui.Zui;
 import zui.Id;
 import arm.util.MeshUtil;
 import arm.nodes.MaterialParser;
 
 class TabMeshes {
 
+	@:access(zui.Zui)
 	public static function draw() {
 		var ui = UITrait.inst.ui;
 		if (ui.tab(UITrait.inst.htab2, "Meshes")) {
-			ui.row([1/4]);
+			ui.row([1/4, 1/4]);
 
 			if (ui.button("Import")) Project.importMesh();
 			if (ui.isHovered) ui.tooltip("Import mesh file (" + Config.keymap.file_import_assets  + ")");
 
-			if (ui.panel(Id.handle({selected: false}), "Geometry")) {
-
-				ui.row([1/2,1/2]);
-				if (ui.button("Flip Normals")) {
-					MeshUtil.flipNormals();
-					Context.ddirty = 2;
-				}
-				if (ui.button("Calculate Normals")) {
-					MeshUtil.calcNormals();
-					Context.ddirty = 2;
-				}
-
-				ui.row([1/3, 1/3, 1/3]);
-				if (ui.button("Rotate X")) {
-					MeshUtil.swapAxis(1, 2);
-					Context.ddirty = 2;
-				}
-				if (ui.button("Rotate Y")) {
-					MeshUtil.swapAxis(2, 0);
-					Context.ddirty = 2;
-				}
-				if (ui.button("Rotate Z")) {
-					MeshUtil.swapAxis(0, 1);
-					Context.ddirty = 2;
-				}
+			if (ui.button("Tools...")) {
+				UIMenu.draw(function(ui:Zui) {
+					ui.fill(0, 0, ui._w / ui.SCALE(), ui.t.ELEMENT_H * 6, ui.t.SEPARATOR_COL);
+					ui.text("Tools", Right);
+					if (ui.button("Flip Normals", Left)) {
+						MeshUtil.flipNormals();
+						Context.ddirty = 2;
+					}
+					if (ui.button("Calculate Normals", Left)) {
+						MeshUtil.calcNormals();
+						Context.ddirty = 2;
+					}
+					if (ui.button("Rotate X", Left)) {
+						MeshUtil.swapAxis(1, 2);
+						Context.ddirty = 2;
+					}
+					if (ui.button("Rotate Y", Left)) {
+						MeshUtil.swapAxis(2, 0);
+						Context.ddirty = 2;
+					}
+					if (ui.button("Rotate Z", Left)) {
+						MeshUtil.swapAxis(0, 1);
+						Context.ddirty = 2;
+					}
+				});
 			}
 		}
 	}
