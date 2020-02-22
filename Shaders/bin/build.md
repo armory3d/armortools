2c0eb7b9913147c7cc5595e6418c9833539b9e53
luboslenco <lubos.lenco@gmail.com>
2020-02-22 20:55:27 +0100

    Shader build cleanup

--- Shaders/bin/build.md
+++
@@ -1,5 +0,0 @@
-// https://github.com/luboslenco/krafix_hlslbin
-
-.\hlslbin.exe file_in file_out vs_5_0 -i pos
-.\hlslbin.exe file_in file_out ps_5_0
-.\hlslbin.exe file_in file_out gs_5_0
---
+++ Shaders/voxel_hlsl/src/build.bat
@@ -0,0 +1,3 @@
+..\..\bin\hlslbin.exe armdefault_voxel.vert.hlsl ..\armdefault_voxel.vert.glsl vs_5_0 -i pos
+..\..\bin\hlslbin.exe armdefault_voxel.frag.hlsl ..\armdefault_voxel.frag.glsl ps_5_0
+..\..\bin\hlslbin.exe armdefault_voxel.geom.hlsl ..\armdefault_voxel.geom.glsl gs_5_0
