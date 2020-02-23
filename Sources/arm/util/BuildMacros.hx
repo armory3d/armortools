d2458578d794736ab7696a8faeea616b7237e003
luboslenco <lubos.lenco@gmail.com>
2020-02-23 19:44:17 +0100

    Cleanup unreachable message

--- Sources/arm/util/BuildMacros.hx
+++ Sources/arm/util/BuildMacros.hx
@@ -10,11 +10,6 @@ class BuildMacros {
 
 	macro public static function sha():ExprOf<String> {
 		var proc = new sys.io.Process("git", ["log", "--pretty=format:'%h'", "-n", "1"]);
-		try {
-			return Context.makeExpr(proc.stdout.readLine(), Context.currentPos());
-		}
-		catch (e:Dynamic) {
-			throw "- use 'git clone --recursive https://github.com/armory3d/armorpaint' to compile armorpaint";
-		}
+		return Context.makeExpr(proc.stdout.readLine(), Context.currentPos());
 	}
 }
