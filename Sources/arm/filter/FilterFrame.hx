1bbe153bc15f9cf35ebb1972c7f970870ec1f559
Luffy <luffy@outlook.com>
2020-08-25 17:25:30 +0800

    修复UI刷新BUG，并添加缩进功能

--- Sources/arm/filter/FilterFrame.hx
+++ Sources/arm/filter/FilterFrame.hx
@@ -7,37 +7,33 @@ import zui.Id;
 
 class FilterFrame extends FilterBase
 {
-    public static var filterNamesTr: Array<String> = [];
-    public var filter: FilterBase = null;
-    var ch = Id.handle();
+    var filter: FilterBase = null;
+    var ch = new Handle();
 
     public function new() {
         this.name = "None";
-
-        // tr names
-        if (filterNamesTr.length == 0) {
-            for (i in 0...FilterFactory.filterNames.length) {
-                filterNamesTr.push(tr(FilterFactory.filterNames[i]));
-            }
-        }
     }
 
     override public function draw() {
         var ui = UISidebar.inst.ui;
         var title = filter == null ? name : filter.name;
         if (ui.panel(h, tr(title))) {
+            ui.indent(false);
             ui.row([4/5, 1/5]);
 
             // header
             // var ch = new Handle({position: FilterFactory.filterNames.indexOf(title)});
             ch.position = FilterFactory.filterNames.indexOf(title);
-            ui.combo(ch, filterNamesTr, tr("Type"), true, zui.Zui.Align.Right);
+            ui.combo(ch, FilterFactory.filterNames, tr("Type"), true, zui.Zui.Align.Right);
             if (ch.changed) {
                 filter = FilterFactory.CreateFilterByIndex(ch.position);
             }
 
             ui.button(tr("Remove"));
             if (filter != null) filter.draw();
+
+            ui.unindent(false);
+            ui.separator(4, true);
         }
     }
 
