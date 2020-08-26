abe7b9da1a063698d9c1238b78c2b9cbcefdee72
Luffy <luffy@outlook.com>
2020-08-26 15:58:57 +0800

    添加对比度、饱和度、伽玛调整

--- Sources/arm/filter/ColorCorrection.hx
+++ Sources/arm/filter/ColorCorrection.hx
@@ -8,11 +8,10 @@ class ColorCorrection extends FilterBase
 {
 
     public var gh = new Handle();
-    public var ch = new Handle();
+    public var ch = new Handle({value: 1});
     public var bh = new Handle();
-    public var sh = new Handle();
+    public var sh = new Handle({value: 1});
     public var hh = new Handle();
-    public var oh = new Handle({value: 100});
 
     public function new() {
         this.name = "Color Correction";
@@ -23,24 +22,38 @@ class ColorCorrection extends FilterBase
         var ui = UISidebar.inst.ui;
 
         ui.slider(gh, tr("Gama"), -3, 3, true);
-        ui.slider(ch, tr("Contrast"), -2, 2, true);
         ui.slider(bh, tr("Brightness"), -1, 1, true);
-        ui.slider(sh, tr("Saturation"), -1, 5, true);
+        ui.slider(ch, tr("Contrast"), 0, 2, true);
+        ui.slider(sh, tr("Saturation"), 0, 5, true);
         ui.slider(hh, tr("Hue Shift"), -180, 180, true);
-        ui.slider(oh, tr("Opacity"), 0, 100, true);
 
         if (gh.changed) { MakeMaterial.parseMeshMaterial(); }
         if (ch.changed) { MakeMaterial.parseMeshMaterial(); }
         if (bh.changed) { MakeMaterial.parseMeshMaterial(); }
         if (sh.changed) { MakeMaterial.parseMeshMaterial(); }
         if (hh.changed) { MakeMaterial.parseMeshMaterial(); }
-        if (oh.changed) { MakeMaterial.parseMeshMaterial(); }
     }
 
     override function getShaderText(color: String) : String {
         var ret = "";
-        ret += '${color}.rgb += ${bh.value};';
-        // ret += "basecol = vec3(1, 0, 0);";
+
+        // hue
+
+        // Gama
+        var gamma = (gh.value < 0.0) ? (-gh.value + 1.0) : (1.0 / (gh.value + 1.0));
+        ret += '$color.rgb = pow($color.rgb, vec3($gamma, $gamma, $gamma));';
+
+        //　Contrast
+        ret += '$color.rgb = lerp(vec3(0.5, 0.5, 0.5), $color.rgb, ${ch.value});';
+
+        // Brightness
+        ret += '$color.rgb += ${bh.value};';
+
+        // Saturation
+        var luminance = '$color.r * 0.2125 + $color.g * 0.7154 + $color.b * 0.0721';
+        var luminanceColor = 'vec3($luminance, $luminance, $luminance)';
+        ret += '$color.rgb = lerp($luminanceColor, $color.rgb, ${sh.value});';
+
         return ret;
     }
 
