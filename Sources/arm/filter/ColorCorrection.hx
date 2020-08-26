582fb579c16f40c479f2259d3c12fd5b872631e8
Luffy <luffy@outlook.com>
2020-08-26 17:33:10 +0800

    清理代码，接下来完善UI功能

--- Sources/arm/filter/ColorCorrection.hx
+++ Sources/arm/filter/ColorCorrection.hx
@@ -37,19 +37,7 @@ class ColorCorrection extends FilterBase
     override function getShaderText(color: String) : String {
         var ret = "";
 
-        // hue
-        // var root3 = "vec3(0.57735, 0.57735, 0.57735)";
-        // var half_angle = '0.5 * radians(${hh.value})';
-        // var quat = 'vec4( ($root3 * sin($half_angle)), cos($half_angle) )';
-        // var diag = '0.5 - ($quat.rgb * $quat.rgb)';
-        // var a = '($quat.gbr * $quat.brg) + ($quat.a * $quat.rgb)';
-        // var b = '($quat.gbr * $quat.brg) - ($quat.a * $quat.rgb)';
-
-        // var rot_matrix = 'vec3x3(
-        //                         2.0*vec3($diag.x, $b.z, $a,y), 
-        //                         2.0*vec3($a.z, $diag.y, $b.x), 
-        //                         2.0*vec3($b.y, $a.x, $diag.z))';
-        // ret += '$color.rgb = mul($rot_matrix, $color.rgb);';
+        // hue & Brightness & Saturation
         var vec = 'vec4(${hh.value}, ${sh.value}, ${bh.value}, 0)';
         ret += '$color.rgb = hue_sat($color.rgb, $vec);';
 
@@ -59,14 +47,6 @@ class ColorCorrection extends FilterBase
 
         //　Contrast
         ret += '$color.rgb = lerp(vec3(0.5, 0.5, 0.5), $color.rgb, ${ch.value});';
-
-        // Brightness
-        // ret += '$color.rgb += ${bh.value};';
-
-        // Saturation
-        // var luminance = '$color.r * 0.2125 + $color.g * 0.7154 + $color.b * 0.0721';
-        // var luminanceColor = 'vec3($luminance, $luminance, $luminance)';
-        // ret += '$color.rgb = lerp($luminanceColor, $color.rgb, ${sh.value});';
 
         return ret;
     }
