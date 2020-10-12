bdecfa136f5674458f9495fa268a82d0e9828ff7
luboslenco <lubos.lenco@gmail.com>
2020-10-12 09:41:08 +0200

    Use layer mask for texture synthesis

--- Assets/plugins/texsynth.js
+++ Assets/plugins/texsynth.js
@@ -1,4 +1,7 @@
+// Example-based texture synthesis written in Rust
 // https://github.com/EmbarkStudios/texture-synthesis
+
+// texsynth library is linked with armorcore until multi-threaded execution is supported via webassembly
 // https://github.com/armory3d/armorpaint_plugins/tree/master/plugin_texsynth
 
 let plugin = new arm.Plugin();
@@ -19,10 +22,18 @@ function texsynthInpaint(tiling) {
 	let w = arm.Config.getTextureResX();
 	let h = arm.Config.getTextureResY();
 	let l = arm.Context.layer;
+	if (l.texpaint_mask == null) return;
 	let bytes_img = l.texpaint.getPixels().b.bufferValue;
-	let bytes_tile = l.texpaint_pack.getPixels().b.bufferValue;
-	let bytes_out = new arm.Bytes(new ArrayBuffer(w * h * 4))
-	Krom.texsynthInpaint(w, h, bytes_out.b.bufferValue, bytes_img, bytes_tile, tiling);
+	let bytes_mask = l.texpaint_mask.getPixels().b.bufferValue;
+	let view_img = new Uint8Array(bytes_img);
+	let view_mask = new Uint8Array(bytes_mask);
+	for (let i = 0; i < view_img.length / 4; ++i) {
+		view_img[i * 4 + 3] = view_mask[i];
+	}
+
+	let bytes_out = new arm.Bytes(new ArrayBuffer(w * h * 4));
+	Krom.texsynthInpaint(w, h, bytes_out.b.bufferValue, bytes_img, tiling);
+
 	let image = arm.Image.fromBytes(bytes_out, w, h);
 	var asset = {name: "tex_synth.png", file: "/tex_synth.png", id: arm.Project.assetId++};
 	iron.Data.cachedImages.h[asset.file] = image;
