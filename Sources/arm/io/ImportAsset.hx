8717001c29fc2ef8eb37e8f94e5226c32f1b0629
luboslenco <lubos.lenco@gmail.com>
2021-01-11 21:58:15 +0100

    Importing asset from cloud

--- Sources/arm/io/ImportAsset.hx
+++ Sources/arm/io/ImportAsset.hx
@@ -1,6 +1,7 @@
 package arm.io;
 
 import arm.sys.Path;
+import arm.sys.File;
 import arm.ui.UINodes;
 import arm.ui.UIBox;
 import arm.Project;
@@ -8,6 +9,18 @@ import arm.Project;
 class ImportAsset {
 
 	public static function run(path: String, dropX = -1.0, dropY = -1.0, showBox = true) {
+
+		if (path.startsWith("cloud")) {
+			var abs = Path.workingDir() + Path.sep + path;
+			if (!File.exists(abs)) {
+				var fileDir = Krom.getFilesLocation() + Path.sep + path.substr(0, path.lastIndexOf(Path.sep));
+				File.createDirectory(fileDir);
+				var url = Config.raw.server + "/" + path;
+				File.download(url, path);
+			}
+			path = abs;
+		}
+
 		if (Path.isMesh(path)) {
 			showBox ? Project.importMeshBox(path) : ImportMesh.run(path);
 			if (dropX > 0) UIBox.clickToHide = false; // Prevent closing when going back to window after drag and drop
