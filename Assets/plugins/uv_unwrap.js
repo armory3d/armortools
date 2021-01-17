880ee49d43df2814cc83856895039df4c9a3950c
luboslenco <lubos.lenco@gmail.com>
2021-01-17 13:14:23 +0100

    Update uv_unwrap plugin

--- Assets/plugins/uv_unwrap.js
+++ Assets/plugins/uv_unwrap.js
@@ -9,7 +9,21 @@ Module["instantiateWasm"] = function(imports, successCallback) {
 };
 
 // emscripten-generated glue
-var b;b=b||("undefined"!=typeof Module?Module:{});var k,h={};for(k in b)b.hasOwnProperty(k)&&(h[k]=b[k]);var m,l="./this.program";m=function(t){return"function"==typeof readbuffer?new Uint8Array(readbuffer(t)):("object"==typeof(t=read(t,"binary"))||n("Assertion failed: undefined"),t)},"undefined"!=typeof print&&("undefined"==typeof console&&(console={}),console.log=print,console.warn=console.error="undefined"!=typeof printErr?printErr:print);var r,p=b.print||console.log.bind(console),q=b.printErr||console.warn.bind(console);for(k in h)h.hasOwnProperty(k)&&(b[k]=h[k]);h=null,b.thisProgram&&(l=b.thisProgram),b.wasmBinary&&(r=b.wasmBinary),"object"!=typeof WebAssembly&&q("no native wasm support detected");var t,A,B,y,C,u=new WebAssembly.Table({initial:9,maximum:9,element:"anyfunc"}),v=!1,w="undefined"!=typeof TextDecoder?new TextDecoder("utf8"):void 0;function x(n){if(n){for(var t=y,e=n+void 0,r=n;t[r]&&!(e<=r);)++r;if(16<r-n&&t.subarray&&w)n=w.decode(t.subarray(n,r));else{for(e="";n<r;){var o=t[n++];if(128&o){var a=63&t[n++];if(192==(224&o))e+=String.fromCharCode((31&o)<<6|a);else{var i=63&t[n++];(o=224==(240&o)?(15&o)<<12|a<<6|i:(7&o)<<18|a<<12|i<<6|63&t[n++])<65536?e+=String.fromCharCode(o):(o-=65536,e+=String.fromCharCode(55296|o>>10,56320|1023&o))}}else e+=String.fromCharCode(o)}n=e}}else n="";return n}function z(n){return 0<n%65536&&(n+=65536-n%65536),n}function D(n){A=n,b.HEAP8=B=new Int8Array(n),b.HEAP16=new Int16Array(n),b.HEAP32=C=new Int32Array(n),b.HEAPU8=y=new Uint8Array(n),b.HEAPU16=new Uint16Array(n),b.HEAPU32=new Uint32Array(n),b.HEAPF32=new Float32Array(n),b.HEAPF64=new Float64Array(n)}"undefined"!=typeof TextDecoder&&new TextDecoder("utf-16le");var E=b.TOTAL_MEMORY||16777216;function F(n){for(;0<n.length;){var t=n.shift();if("function"==typeof t)t();else{var e=t.A;"number"==typeof e?void 0===t.w?b.dynCall_v(e):b.dynCall_vi(e,t.w):e(void 0===t.w?null:t.w)}}}(t=b.wasmMemory?b.wasmMemory:new WebAssembly.Memory({initial:E/65536}))&&(A=t.buffer),E=A.byteLength,D(A),C[2124]=5251536;var G=[],H=[],I=[],K=[];function L(){var n=b.preRun.shift();G.unshift(n)}var M=0,N=null,O=null;function n(n){throw b.onAbort&&b.onAbort(n),p(n),q(n),v=!0,new WebAssembly.RuntimeError("abort("+n+"). Build with -s ASSERTIONS=1 for more info.")}function P(){var n=Q;return String.prototype.startsWith?n.startsWith("data:application/octet-stream;base64,"):0===n.indexOf("data:application/octet-stream;base64,")}b.preloadedImages={},b.preloadedAudios={};var Q="uv_unwrap.wasm";if(!P()){var R=Q;Q=b.locateFile?b.locateFile(R,""):""+R}function S(){return new Promise(function(t){n:{try{if(r){var e=new Uint8Array(r);break n}if(m){e=m(Q);break n}throw"both async and sync fetching of the wasm failed"}catch(t){n(t)}e=void 0}t(e)})}H.push({A:function(){aa()}});var T={};function U(){if(!V){var n,t={USER:"web_user",LOGNAME:"web_user",PATH:"/",PWD:"/",HOME:"/home/web_user",LANG:("object"==typeof navigator&&navigator.languages&&navigator.languages[0]||"C").replace("-","_")+".UTF-8",_:l};for(n in T)t[n]=T[n];var e=[];for(n in t)e.push(n+"="+t[n]);V=e}return V}var V,W={a:function(t,e,r,o){n("Assertion failed: "+x(t)+", at: "+[e?x(e):"unknown filename",r,o?x(o):"unknown function"])},e:function(n,t,e){y.set(y.subarray(t,t+e),n)},d:function(n){if(2147418112<n)return!1;for(var e=Math.max(B.length,16777216);e<n;)e=e<=536870912?z(2*e):Math.min(z((3*e+2147483648)/4),2147418112);n:{try{t.grow(e-A.byteLength+65535>>16),D(t.buffer);var r=1;break n}catch(n){}r=void 0}return!!r},f:function(r,o){var a=0;return U().forEach(function(n,t){var e=o+a;for(t=C[r+4*t>>2]=e,e=0;e<n.length;++e)B[t++>>0]=n.charCodeAt(e);B[t>>0]=0,a+=n.length+1}),0},g:function(n,t){var e=U();C[n>>2]=e.length;var r=0;return e.forEach(function(n){r+=n.length+1}),C[t>>2]=r,0},c:function(){q("missing function: iprintf"),n(-1)},memory:t,b:function(n){var t=(n=x(n)).substr(0);return"\n"===t[t.length-1]&&(t=t.substr(0,t.length-1)),p(t),n.length},table:u},X=function(){function t(n){b.asm=n.exports,M--,b.monitorRunDependencies&&b.monitorRunDependencies(M),0==M&&(null!==N&&(clearInterval(N),N=null),O&&(n=O,O=null,n()))}function e(n){t(n.instance)}function o(t){return S().then(function(n){return WebAssembly.instantiate(n,a)}).then(t,function(t){q("failed to asynchronously prepare wasm: "+t),n(t)})}var a={env:W,wasi_unstable:W};if(M++,b.monitorRunDependencies&&b.monitorRunDependencies(M),b.instantiateWasm)try{return b.instantiateWasm(a,t)}catch(n){return q("Module.instantiateWasm callback failed with error: "+n),!1}return function(){if(r||"function"!=typeof WebAssembly.instantiateStreaming||P()||"function"!=typeof fetch)return o(e);fetch(Q,{credentials:"same-origin"}).then(function(n){return WebAssembly.instantiateStreaming(n,a).then(e,function(n){q("wasm streaming compile failed: "+n),q("falling back to ArrayBuffer instantiation"),o(e)})})}(),{}}();b.asm=X;var Y,aa=b.___wasm_call_ctors=function(){return b.asm.h.apply(null,arguments)};function Z(){function n(){if(!Y&&(Y=!0,!v)){if(F(H),F(I),b.onRuntimeInitialized&&b.onRuntimeInitialized(),b.postRun)for("function"==typeof b.postRun&&(b.postRun=[b.postRun]);b.postRun.length;){var n=b.postRun.shift();K.unshift(n)}F(K)}}if(!(0<M)){if(b.preRun)for("function"==typeof b.preRun&&(b.preRun=[b.preRun]);b.preRun.length;)L();F(G),0<M||(b.setStatus?(b.setStatus("Running..."),setTimeout(function(){setTimeout(function(){b.setStatus("")},1),n()},1)):n())}}if(b._setVertexCount=function(){return b.asm.i.apply(null,arguments)},b._setIndexCount=function(){return b.asm.j.apply(null,arguments)},b._setPositions=function(){return b.asm.k.apply(null,arguments)},b._setNormals=function(){return b.asm.l.apply(null,arguments)},b._setIndices=function(){return b.asm.m.apply(null,arguments)},b._getVertexCount=function(){return b.asm.n.apply(null,arguments)},b._getIndexCount=function(){return b.asm.o.apply(null,arguments)},b._getPositions=function(){return b.asm.p.apply(null,arguments)},b._getNormals=function(){return b.asm.q.apply(null,arguments)},b._getUVs=function(){return b.asm.r.apply(null,arguments)},b._getIndices=function(){return b.asm.s.apply(null,arguments)},b._unwrap=function(){return b.asm.t.apply(null,arguments)},b._destroy=function(){return b.asm.u.apply(null,arguments)},b.dynCall_vi=function(){return b.asm.v.apply(null,arguments)},b.asm=X,O=function n(){Y||Z(),Y||(O=n)},b.run=Z,b.preInit)for("function"==typeof b.preInit&&(b.preInit=[b.preInit]);0<b.preInit.length;)b.preInit.pop()();Z();
+var a;a||(a=typeof Module !== 'undefined' ? Module : {});var h={},k;for(k in a)a.hasOwnProperty(k)&&(h[k]=a[k]);var l;l=function(b){if("function"===typeof readbuffer)return new Uint8Array(readbuffer(b));b=read(b,"binary");"object"===typeof b||m("Assertion failed: undefined");return b};"undefined"!==typeof print&&("undefined"===typeof console&&(console={}),console.log=print,console.warn=console.error="undefined"!==typeof printErr?printErr:print);
+var n=a.print||console.log.bind(console),p=a.printErr||console.warn.bind(console);for(k in h)h.hasOwnProperty(k)&&(a[k]=h[k]);h=null;var q;a.wasmBinary&&(q=a.wasmBinary);var noExitRuntime;a.noExitRuntime&&(noExitRuntime=a.noExitRuntime);"object"!==typeof WebAssembly&&m("no native wasm support detected");var r,t=!1,v="undefined"!==typeof TextDecoder?new TextDecoder("utf8"):void 0;
+function w(b,c,e){var f=c+e;for(e=c;b[e]&&!(e>=f);)++e;if(16<e-c&&b.subarray&&v)return v.decode(b.subarray(c,e));for(f="";c<e;){var d=b[c++];if(d&128){var g=b[c++]&63;if(192==(d&224))f+=String.fromCharCode((d&31)<<6|g);else{var u=b[c++]&63;d=224==(d&240)?(d&15)<<12|g<<6|u:(d&7)<<18|g<<12|u<<6|b[c++]&63;65536>d?f+=String.fromCharCode(d):(d-=65536,f+=String.fromCharCode(55296|d>>10,56320|d&1023))}}else f+=String.fromCharCode(d)}return f}function x(b){return b?w(y,b,void 0):""}var z,y,A;
+function B(){var b=r.buffer;z=b;a.HEAP8=new Int8Array(b);a.HEAP16=new Int16Array(b);a.HEAP32=A=new Int32Array(b);a.HEAPU8=y=new Uint8Array(b);a.HEAPU16=new Uint16Array(b);a.HEAPU32=new Uint32Array(b);a.HEAPF32=new Float32Array(b);a.HEAPF64=new Float64Array(b)}var C,G=[],H=[],I=[],J=[];H.push({v:function(){K()}});function L(){var b=a.preRun.shift();G.unshift(b)}var M=0,N=null,O=null;a.preloadedImages={};a.preloadedAudios={};
+function m(b){if(a.onAbort)a.onAbort(b);p(b);t=!0;throw new WebAssembly.RuntimeError("abort("+b+"). Build with -s ASSERTIONS=1 for more info.");}function P(){var b=Q;return String.prototype.startsWith?b.startsWith("data:application/octet-stream;base64,"):0===b.indexOf("data:application/octet-stream;base64,")}var Q="xatlas.wasm";if(!P()){var R=Q;Q=a.locateFile?a.locateFile(R,""):""+R}
+function T(){return Promise.resolve().then(function(){a:{var b=Q;try{if(b==Q&&q){var c=new Uint8Array(q);break a}if(l){c=l(b);break a}throw"both async and sync fetching of the wasm failed";}catch(e){m(e)}c=void 0}return c})}function U(b){for(;0<b.length;){var c=b.shift();if("function"==typeof c)c(a);else{var e=c.v;"number"===typeof e?void 0===c.u?C.get(e)():C.get(e)(c.u):e(void 0===c.u?null:c.u)}}}
+var V=[null,[],[]],W={a:function(b,c,e,f){m("Assertion failed: "+x(b)+", at: "+[c?x(c):"unknown filename",e,f?x(f):"unknown function"])},d:function(b,c,e){y.copyWithin(b,c,c+e)},b:function(b){b>>>=0;var c=y.length;if(2147483648<b)return!1;for(var e=1;4>=e;e*=2){var f=c*(1+.2/e);f=Math.min(f,b+100663296);f=Math.max(16777216,b,f);0<f%65536&&(f+=65536-f%65536);a:{try{r.grow(Math.min(2147483648,f)-z.byteLength+65535>>>16);B();var d=1;break a}catch(g){}d=void 0}if(d)return!0}return!1},c:function(b,c,e,
+f){for(var d=0,g=0;g<e;g++){for(var u=A[c+8*g>>2],S=A[c+(8*g+4)>>2],D=0;D<S;D++){var E=y[u+D],F=V[b];0===E||10===E?((1===b?n:p)(w(F,0)),F.length=0):F.push(E)}d+=S}A[f>>2]=d;return 0}};
+(function(){function b(d){a.asm=d.exports;r=a.asm.e;B();C=a.asm.f;M--;a.monitorRunDependencies&&a.monitorRunDependencies(M);0==M&&(null!==N&&(clearInterval(N),N=null),O&&(d=O,O=null,d()))}function c(d){b(d.instance)}function e(d){return T().then(function(g){return WebAssembly.instantiate(g,f)}).then(d,function(g){p("failed to asynchronously prepare wasm: "+g);m(g)})}var f={a:W};M++;a.monitorRunDependencies&&a.monitorRunDependencies(M);if(a.instantiateWasm)try{return a.instantiateWasm(f,b)}catch(d){return p("Module.instantiateWasm callback failed with error: "+
+d),!1}(function(){return q||"function"!==typeof WebAssembly.instantiateStreaming||P()||"function"!==typeof fetch?e(c):fetch(Q,{credentials:"same-origin"}).then(function(d){return WebAssembly.instantiateStreaming(d,f).then(c,function(g){p("wasm streaming compile failed: "+g);p("falling back to ArrayBuffer instantiation");return e(c)})})})();return{}})();var K=a.___wasm_call_ctors=function(){return(K=a.___wasm_call_ctors=a.asm.g).apply(null,arguments)};
+a._setVertexCount=function(){return(a._setVertexCount=a.asm.h).apply(null,arguments)};a._setIndexCount=function(){return(a._setIndexCount=a.asm.i).apply(null,arguments)};a._setPositions=function(){return(a._setPositions=a.asm.j).apply(null,arguments)};a._setNormals=function(){return(a._setNormals=a.asm.k).apply(null,arguments)};a._setIndices=function(){return(a._setIndices=a.asm.l).apply(null,arguments)};a._getVertexCount=function(){return(a._getVertexCount=a.asm.m).apply(null,arguments)};
+a._getIndexCount=function(){return(a._getIndexCount=a.asm.n).apply(null,arguments)};a._getPositions=function(){return(a._getPositions=a.asm.o).apply(null,arguments)};a._getNormals=function(){return(a._getNormals=a.asm.p).apply(null,arguments)};a._getUVs=function(){return(a._getUVs=a.asm.q).apply(null,arguments)};a._getIndices=function(){return(a._getIndices=a.asm.r).apply(null,arguments)};a._unwrap=function(){return(a._unwrap=a.asm.s).apply(null,arguments)};
+a._destroy=function(){return(a._destroy=a.asm.t).apply(null,arguments)};var X;O=function Y(){X||Z();X||(O=Y)};
+function Z(){function b(){if(!X&&(X=!0,a.calledRun=!0,!t)){U(H);U(I);if(a.onRuntimeInitialized)a.onRuntimeInitialized();if(a.postRun)for("function"==typeof a.postRun&&(a.postRun=[a.postRun]);a.postRun.length;){var c=a.postRun.shift();J.unshift(c)}U(J)}}if(!(0<M)){if(a.preRun)for("function"==typeof a.preRun&&(a.preRun=[a.preRun]);a.preRun.length;)L();U(G);0<M||(a.setStatus?(a.setStatus("Running..."),setTimeout(function(){setTimeout(function(){a.setStatus("")},1);b()},1)):b())}}a.run=Z;
+if(a.preInit)for("function"==typeof a.preInit&&(a.preInit=[a.preInit]);0<a.preInit.length;)a.preInit.pop()();noExitRuntime=!0;Z();
 //
 
 let plugin = new arm.Plugin();
@@ -27,11 +41,11 @@ plugin.drawUI = function(ui) {
 				let vertexCount = positions.length / 4;
 				let indexCount = indices.length;
 
-				b._setVertexCount(vertexCount);
-				b._setIndexCount(indexCount);
-				let pa = new Float32Array(t.buffer, b._setPositions(), vertexCount * 3);
-				let na = new Float32Array(t.buffer, b._setNormals(), vertexCount * 3);
-				let ia = new Uint32Array(t.buffer, b._setIndices(), indexCount);
+				a._setVertexCount(vertexCount);
+				a._setIndexCount(indexCount);
+				let pa = new Float32Array(r.buffer, a._setPositions(), vertexCount * 3);
+				let na = new Float32Array(r.buffer, a._setNormals(), vertexCount * 3);
+				let ia = new Uint32Array(r.buffer, a._setIndices(), indexCount);
 
 				let inv = 1 / 32767;
 
@@ -47,14 +61,14 @@ plugin.drawUI = function(ui) {
 					ia[i] = indices[i];
 				}
 
-				b._unwrap();
+				a._unwrap();
 
-				vertexCount = b._getVertexCount();
-				indexCount = b._getIndexCount();
-				pa = new Float32Array(t.buffer, b._getPositions(), vertexCount * 3);
-				na = new Float32Array(t.buffer, b._getNormals(), vertexCount * 3);
-				let ua = new Float32Array(t.buffer, b._getUVs(), vertexCount * 2);
-				ia = new Uint32Array(t.buffer, b._getIndices(), indexCount);
+				vertexCount = a._getVertexCount();
+				indexCount = a._getIndexCount();
+				pa = new Float32Array(r.buffer, a._getPositions(), vertexCount * 3);
+				na = new Float32Array(r.buffer, a._getNormals(), vertexCount * 3);
+				let ua = new Float32Array(r.buffer, a._getUVs(), vertexCount * 2);
+				ia = new Uint32Array(r.buffer, a._getIndices(), indexCount);
 
 				let pa16 = new Int16Array(vertexCount * 4);
 				let na16 = new Int16Array(vertexCount * 2);
@@ -85,10 +99,10 @@ plugin.drawUI = function(ui) {
 				geom.ready = false;
 				geom.build();
 
-				b._destroy();
+				a._destroy();
 			}
 
-			arm.Context.mergedObject = null;
+			arm.MeshUtil.mergeMesh();
 		}
 	}
 }
