c16881a17de9d2c902f8ad2d5b01930f8e26ed4a
luboslenco <lubos.lenco@gmail.com>
2021-01-19 16:43:18 +0100

    Native cloud requests

--- Libraries/syslib/Sources/arm/sys/File.hx
+++ Libraries/syslib/Sources/arm/sys/File.hx
@@ -17,6 +17,7 @@ class File {
 	#end
 
 	static var cloud: Map<String, Array<String>> = null;
+	static var cloudSizes: Map<String, Int> = null;
 
 	public static function readDirectory(path: String, foldersOnly = false): Array<String> {
 		if (path.startsWith("cloud")) {
@@ -65,9 +66,16 @@ class File {
 		return Krom.fileExists(path);
 	}
 
-	public static function download(url: String, dstPath: String) {
+	public static function download(url: String, dstPath: String, size: Null<Int> = null) {
 		#if krom_windows
-		Krom.sysCommand('powershell -c "Invoke-WebRequest -Uri ' + url + " -OutFile '" + dstPath + "'");
+		if (size == null) {
+			Krom.sysCommand('powershell -c "Invoke-WebRequest -Uri ' + url + " -OutFile '" + dstPath + "'");
+		}
+		else {
+			Krom.httpRequest(url, size, function(ab: js.lib.ArrayBuffer) {
+				if (ab != null) Krom.fileSaveBytes(dstPath, ab);
+			});
+		}
 		#else
 		Krom.sysCommand("curl " + url + " -o " + dstPath);
 		#end
@@ -89,8 +97,11 @@ class File {
 		if (!File.exists(dest)) {
 			var fileDir = Krom.getFilesLocation() + Path.sep + path.substr(0, path.lastIndexOf(Path.sep));
 			File.createDirectory(fileDir);
+			#if krom_windows
+			path = path.replace("\\", "/");
+			#end
 			var url = Config.raw.server + "/" + path;
-			File.download(url, path);
+			File.download(url, path, cloudSizes.get(path));
 			if (!File.exists(dest)) {
 				Log.error(Strings.error5());
 				return null;
@@ -101,7 +112,9 @@ class File {
 
 	static function initCloud() {
 		cloud = [];
+		cloudSizes = [];
 		var files: Array<String> = [];
+		var sizes: Array<Int> = [];
 		var bytes = File.downloadBytes(Config.raw.server);
 		var dataPath = Path.data().startsWith(Path.workingDir()) ? Path.data() : Path.workingDir() + Path.sep + Path.data();
 		if (!File.exists(dataPath + Path.sep + "download.bin")) {
@@ -113,19 +126,26 @@ class File {
 			for (k in e.elementsNamed("Key")) {
 				files.push(k.firstChild().nodeValue);
 			}
+			for (k in e.elementsNamed("Size")) {
+				sizes.push(Std.parseInt(k.firstChild().nodeValue));
+			}
 		}
 		for (file in files) {
 			if (Path.isFolder(file)) {
 				cloud.set(file.substr(0, file.length - 1), []);
 			}
 		}
-		for (file in files) {
+		for (i in 0...files.length) {
+			var file = files[i];
 			var nested = file.indexOf("/") != file.lastIndexOf("/");
 			if (nested) {
 				var delim = Path.isFolder(file) ? file.substr(0, file.length - 1).lastIndexOf("/") : file.lastIndexOf("/");
 				var parent = file.substr(0, delim);
 				var child = Path.isFolder(file) ? file.substring(delim + 1, file.length - 1)  : file.substr(delim + 1);
 				cloud.get(parent).push(child);
+				if (!Path.isFolder(file)) {
+					cloudSizes.set(file, sizes[i]);
+				}
 			}
 		}
 	}
