867add3ce01d6fd6084dce6b1d9a09ac94964e14
Lubos Lenco <lubos.lenco@gmail.com>
2021-06-19 19:41:37 +0200

    Fix decal layers when using forward path

--- Sources/arm/render/RenderPathForward.hx
+++ Sources/arm/render/RenderPathForward.hx
@@ -78,7 +78,7 @@ class RenderPathForward {
 				RenderPathDeferred.copyToGbuffer();
 			}
 		}
-		LineDraw.render(path.currentG);
+		LineDraw.render(currentG);
 	}
 
 	public static function drawForward(eye = false, output = "", gbuffer0 = "gbuffer0", gbuffer1 = "gbuffer1", gbuffer2 = "gbuffer2", buf = "buf", bufa = "bufa", taa = "taa", taa2 = "taa2") {
