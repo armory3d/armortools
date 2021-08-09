458238a4aa7a86d5dbd822788063bc83272b1a30
Lubos Lenco <lubos.lenco@gmail.com>
2021-08-09 19:16:45 +0200

    Add kincflags.js

---
+++ kincflags.js
@@ -0,0 +1,13 @@
+
+// Imported by armorcore/kincfile.js
+module.exports = {
+	set_flags: function (flags) {
+		flags.name = 'ArmorPaint';
+		flags.package = 'org.armorpaint';
+		flags.with_d3dcompiler = true;
+		flags.with_nfd = true;
+		flags.with_tinydir = true;
+		flags.with_zlib = true;
+		flags.with_stb_image_write = true;
+	}
+};
