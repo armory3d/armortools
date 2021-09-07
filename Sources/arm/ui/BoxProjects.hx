69d1c4c77646ad50430065987c813d45d3b9944d
Lubos Lenco <lubos.lenco@gmail.com>
2021-09-07 15:38:00 +0200

    Improve project handling on ios and android

--- Sources/arm/ui/BoxProjects.hx
+++ Sources/arm/ui/BoxProjects.hx
@@ -28,31 +28,66 @@ class BoxProjects {
 				ui.endSticky();
 				ui.separator(3, false);
 
-				for (path in Config.raw.recent_projects) {
+				var slotw = Std.int(300 * ui.SCALE());
+				var num = Std.int(kha.System.windowWidth() / slotw);
+				var recent_projects = Config.raw.recent_projects;
 
-					#if krom_ios
-					var documentDirectory = Krom.saveDialog("", "");
-					documentDirectory = documentDirectory.substr(0, documentDirectory.length - 8); // Strip /'untitled'
-					path = documentDirectory + path;
-					#end
+				for (row in 0...Std.int(Math.ceil(recent_projects.length / num))) {
+					ui.row([for (i in 0...num) 1 / num]);
 
-					var iconPath = path.substr(0, path.length - 4) + "_icon.png";
-					if (iconMap == null) iconMap = [];
-					var icon = iconMap.get(iconPath);
-					if (icon == null) {
-						iron.data.Data.getImage(iconPath, function(image: kha.Image) {
-							icon = image;
-							iconMap.set(iconPath, icon);
-						});
+					for (j in 0...num) {
+						var i = j + row * num;
+						if (i >= recent_projects.length) {
+							var imgw = Std.int(256 * ui.SCALE());
+							@:privateAccess ui.endElement(imgw);
+							continue;
+						}
+
+						var path = recent_projects[i];
+
+						#if krom_ios
+						var documentDirectory = Krom.saveDialog("", "");
+						documentDirectory = documentDirectory.substr(0, documentDirectory.length - 8); // Strip /'untitled'
+						path = documentDirectory + path;
+						#end
+
+						var iconPath = path.substr(0, path.length - 4) + "_icon.png";
+						if (iconMap == null) iconMap = [];
+						var icon = iconMap.get(iconPath);
+						if (icon == null) {
+							iron.data.Data.getImage(iconPath, function(image: kha.Image) {
+								icon = image;
+								iconMap.set(iconPath, icon);
+							});
+						}
+
+						ui.fill(0, 0, 256, 256, ui.t.SEPARATOR_COL);
+						var state = ui.image(icon);
+						if (state == Released) {
+							iron.App.notifyOnInit(function() {
+								ImportArm.runProject(path);
+							});
+							UIBox.show = false;
+						}
+						if (ui.isHovered && ui.inputReleasedR) {
+							UIMenu.draw(function(ui: Zui) {
+								var name = path.substr(Project.filepath.lastIndexOf("/") + 1);
+								ui.text(name, Right, ui.t.HIGHLIGHT_COL);
+								if (ui.button(tr("Duplicate"), Left)) {
+
+								}
+								if (ui.button(tr("Delete"), Left)) {
+									iron.App.notifyOnInit(function() {
+										arm.sys.File.delete(path);
+										recent_projects.splice(i, 1);
+									});
+								}
+							}, 3);
+						}
 					}
 
-					var state = ui.image(icon);
-					if (state == Released) {
-						ImportArm.runProject(path);
-						UIBox.show = false;
-					}
+					ui._y += 6;
 				}
-
 			}
 		}, 600, 400, null, false);
 
