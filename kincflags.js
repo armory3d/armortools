aa7f29e2ba6b8fc0d05246e4aa1ffccac2427b3a
Lubos Lenco <lubos.lenco@gmail.com>
2022-03-09 17:37:17 +0100

    Switch to kmake

--- kincflags.js
+++ kincflags.js
@@ -1,15 +1,11 @@
 
-// Imported by armorcore/kincfile.js
-module.exports = {
-	set_flags: function(flags, platform, graphics) {
-		flags.name = 'ArmorPaint';
-		flags.package = 'org.armorpaint';
-		flags.with_d3dcompiler = true;
-		flags.with_nfd = true;
-		flags.with_tinydir = true;
-		flags.with_zlib = true;
-		flags.with_stb_image_write = true;
-		flags.with_krafix = graphics === 'vulkan'; // glsl to spirv for vulkan
-		flags.with_plugin_embed = platform === 'ios';
-	}
-};
+// Imported by armorcore/kfile.js
+flags.name = 'ArmorPaint';
+flags.package = 'org.armorpaint';
+flags.with_d3dcompiler = true;
+flags.with_nfd = true;
+flags.with_tinydir = true;
+flags.with_zlib = true;
+flags.with_stb_image_write = true;
+flags.with_krafix = graphics === 'vulkan'; // glsl to spirv for vulkan
+flags.with_plugin_embed = platform === 'ios';
