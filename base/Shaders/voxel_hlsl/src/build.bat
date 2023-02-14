84ea108eb54eceed91b575208b9f0b2b3203d253
luboslenco <lubos.lenco@gmail.com>
2023-02-14 15:01:56 +0100

    Voxel shader cleanup

--- base/Shaders/voxel_hlsl/src/build.bat
+++ base/Shaders/voxel_hlsl/src/build.bat
@@ -1,3 +1,3 @@
-..\..\bin\hlslbin.exe voxel_pos.vert.hlsl ..\voxel_pos.vert.glsl vs_5_0 -i pos
-..\..\bin\hlslbin.exe voxel_pos.frag.hlsl ..\voxel_pos.frag.glsl ps_5_0
-..\..\bin\hlslbin.exe voxel_pos.geom.hlsl ..\voxel_pos.geom.glsl gs_5_0
+.\hlslbin.exe voxel_pos.vert.hlsl ..\voxel_pos.vert.glsl vs_5_0 -i pos
+.\hlslbin.exe voxel_pos.frag.hlsl ..\voxel_pos.frag.glsl ps_5_0
+.\hlslbin.exe voxel_pos.geom.hlsl ..\voxel_pos.geom.glsl gs_5_0
