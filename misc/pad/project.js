69adf45234aef20ae19bd291b421dcaa02483b1c
luboslenco <lubos.lenco@gmail.com>
2023-12-05 00:38:43 +0100

    Unify project files

--- misc/pad/project.js
+++ misc/pad/project.js
@@ -1,4 +1,16 @@
 
+let flags = globalThis.flags;
+flags.name = 'ArmorPad';
+flags.package = 'org.armorpad';
+flags.with_nfd = true;
+flags.with_tinydir = true;
+flags.with_g2 = true;
+flags.with_iron = true;
+flags.with_zui = true;
+flags.on_c_project_created = function(c_project, platform, graphics) {
+	c_project.addDefine('IDLE_SLEEP');
+}
+
 let project = new Project("ArmorPad");
 let root = "../../";
 project.addSources("Sources");
