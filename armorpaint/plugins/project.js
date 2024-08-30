d33d0ef6d154bc2ff258c2dc7213c9beaf400cf4
luboslenco <lubos.lenco@gmail.com>
2024-08-30 14:12:56 +0200

    Add jolt sources

--- armorpaint/plugins/project.js
+++ armorpaint/plugins/project.js
@@ -4,26 +4,15 @@ project.addFile("plugins.c");
 project.addFile("proc_xatlas/**");
 project.addFile("io_svg/**");
 project.addFile("io_usd/**");
+project.addDefine("TINYUSDZ_NO_STB_IMAGE_IMPLEMENTATION");
 project.addFile("io_gltf/**");
 project.addFile("io_fbx/**");
 
-project.addDefine("TINYUSDZ_NO_STB_IMAGE_IMPLEMENTATION");
-
 if (flags.physics) {
 	project.addFile("phys_jolt/phys_jolt.cpp");
+	project.addFile("phys_jolt/Jolt/**");
 	project.addIncludeDir("phys_jolt");
 	project.addDefine("JPH_NO_DEBUG");
-	project.addDefine("JPH_OBJECT_STREAM");
-
-	if (platform === "windows") {
-		project.addLib("phys_jolt/win32/Jolt");
-	}
-	else if (platform === "linux") {
-		project.addLib("Jolt -L" + project.basedir + "/phys_jolt/linux");
-	}
-	else if (platform === "macos") {
-		project.addLib(project.basedir + "/phys_jolt/macos/libJolt.a");
-	}
 }
 
 return project;
