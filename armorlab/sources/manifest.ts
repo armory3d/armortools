74e08bd2528555bbb56b5a7cd8e98fd8919946a1
luboslenco <lubos.lenco@gmail.com>
2024-10-26 15:41:26 +0200

    Bump version

--- armorlab/sources/manifest.ts
+++ armorlab/sources/manifest.ts
@@ -1,6 +1,6 @@
 
 let manifest_title: string = "ArmorLab";
-let manifest_version: string = "0.1";
+let manifest_version: string = "0.2";
 let manifest_url: string = "https://armorlab.org";
 let manifest_url_android: string = "";
 let manifest_url_ios: string = "";
