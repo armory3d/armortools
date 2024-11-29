d9098e9a3993f814d1af996a398c769555a0c981
luboslenco <lubos.lenco@gmail.com>
2024-11-29 12:17:40 +0100

    Cleanup

---
+++ armorsculpt/sources/ui_base_ext.ts
@@ -0,0 +1,29 @@
+
+function ui_base_ext_init_hwnd_tabs(): tab_draw_array_t[] {
+
+	let a0: tab_draw_array_t = [
+		_draw_callback_create(tab_layers_draw),
+		_draw_callback_create(tab_history_draw),
+		_draw_callback_create(tab_plugins_draw)
+	];
+	let a1: tab_draw_array_t = [
+		_draw_callback_create(tab_materials_draw),
+		_draw_callback_create(tab_brushes_draw),
+		_draw_callback_create(tab_particles_draw)
+	];
+	let a2: tab_draw_array_t = [
+		_draw_callback_create(tab_browser_draw),
+		_draw_callback_create(tab_textures_draw),
+		_draw_callback_create(tab_meshes_draw),
+		_draw_callback_create(tab_fonts_draw),
+		_draw_callback_create(tab_script_draw),
+		_draw_callback_create(tab_console_draw),
+		_draw_callback_create(ui_status_draw_version_tab)
+	];
+
+	let r: tab_draw_array_t[] = [];
+	array_push(r, a0);
+	array_push(r, a1);
+	array_push(r, a2);
+	return r;
+}
--- base/sources/ui_base.ts
+++ base/sources/ui_base.ts
@@ -51,74 +51,7 @@ function _draw_callback_create(f: (h: ui_handle_t)=>void): tab_draw_t {
 }
 
 function ui_base_init_hwnd_tabs(): tab_draw_array_t[] {
-	///if is_paint
-	let a0: tab_draw_array_t = [
-		_draw_callback_create(tab_layers_draw),
-		_draw_callback_create(tab_history_draw),
-		_draw_callback_create(tab_plugins_draw)
-		///if is_forge
-		, _draw_callback_create(tab_scene_draw)
-		///end
-	];
-	let a1: tab_draw_array_t = [
-		_draw_callback_create(tab_materials_draw),
-		_draw_callback_create(tab_brushes_draw),
-		_draw_callback_create(tab_particles_draw)
-	];
-	let a2: tab_draw_array_t = [
-		_draw_callback_create(tab_browser_draw),
-		_draw_callback_create(tab_textures_draw),
-		_draw_callback_create(tab_meshes_draw),
-		_draw_callback_create(tab_fonts_draw),
-		_draw_callback_create(tab_swatches_draw),
-		_draw_callback_create(tab_script_draw),
-		_draw_callback_create(tab_console_draw),
-		_draw_callback_create(ui_status_draw_version_tab)
-	];
-	///end
-
-	///if is_sculpt
-	let a0: tab_draw_array_t = [
-		_draw_callback_create(tab_layers_draw),
-		_draw_callback_create(tab_history_draw),
-		_draw_callback_create(tab_plugins_draw)
-	];
-	let a1: tab_draw_array_t = [
-		_draw_callback_create(tab_materials_draw),
-		_draw_callback_create(tab_brushes_draw),
-		_draw_callback_create(tab_particles_draw)
-	];
-	let a2: tab_draw_array_t = [
-		_draw_callback_create(tab_browser_draw),
-		_draw_callback_create(tab_textures_draw),
-		_draw_callback_create(tab_meshes_draw),
-		_draw_callback_create(tab_fonts_draw),
-		_draw_callback_create(tab_script_draw),
-		_draw_callback_create(tab_console_draw),
-		_draw_callback_create(ui_status_draw_version_tab)
-	];
-	///end
-
-	///if is_lab
-	let a0: tab_draw_array_t = [];
-	let a1: tab_draw_array_t = [];
-	let a2: tab_draw_array_t = [
-		_draw_callback_create(tab_browser_draw),
-		_draw_callback_create(tab_textures_draw),
-		_draw_callback_create(tab_meshes_draw),
-		_draw_callback_create(tab_swatches_draw),
-		_draw_callback_create(tab_plugins_draw),
-		_draw_callback_create(tab_script_draw),
-		_draw_callback_create(tab_console_draw),
-		_draw_callback_create(ui_status_draw_version_tab)
-	];
-	///end
-
-	let r: tab_draw_array_t[] = [];
-	array_push(r, a0);
-	array_push(r, a1);
-	array_push(r, a2);
-	return r;
+	return ui_base_ext_init_hwnd_tabs();
 }
 
 function ui_base_init() {
