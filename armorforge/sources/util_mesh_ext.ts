5c629789a2112e045ff746be2b981baf8a2f5885
luboslenco <lubos.lenco@gmail.com>
2024-12-16 19:34:46 +0100

    Forge fixes

--- armorforge/sources/util_mesh_ext.ts
+++ armorforge/sources/util_mesh_ext.ts
@@ -1,8 +1,25 @@
 
+function _util_mesh_unique_data_count(): i32 {
+	let count: i32 = 0;
+	for (let i: i32 = 0; i < project_paint_objects.length; ++i) {
+		let found: bool = false;
+		for (let j: i32 = 0; j < i; ++j) {
+			if (project_paint_objects[i].data == project_paint_objects[j].data) {
+				found = true;
+				break;
+			}
+		}
+		if (!found) {
+			count++;
+		}
+	}
+	return count;
+}
+
 function util_mesh_ext_pack_uvs(texa: i16_array_t) {
     // Scale tex coords into global atlas
 	let atlas_w: i32 = config_get_texture_res();
-	let item_i: i32 = project_paint_objects.length;
+	let item_i: i32 = _util_mesh_unique_data_count();
 	let item_w: i32 = 2048;
 	let atlas_stride: i32 = atlas_w / item_w;
 	let atlas_step: i32 = 32767 / atlas_stride;
