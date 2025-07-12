bb2ea065135cc6fb1927f1449115d272145ede0d
luboslenco <lubos.lenco@gmail.com>
2025-07-12 20:18:21 +0200

    Fix viewport_celshade

--- armorpaint/assets/plugins/viewport_celshade.js
+++ armorpaint/assets/plugins/viewport_celshade.js
@@ -6,7 +6,7 @@ context_set_viewport_shader(function(shader) {
 	// node_shader_add_constant(shader, "light_dir: float3", "_light_dir");
 	node_shader_write_frag(shader, " \
 		var light_dir: float3 = float3(0.5, 0.5, -0.5);\
-		float dotnl = max(dot(n, light_dir), 0.0); \
+		var dotnl: float = max(dot(n, light_dir), 0.0); \
 		output_color = basecol * step(0.5, dotnl) + basecol; \
 	");
 });
