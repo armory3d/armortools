8addf47c7752e4bbef96689d71c821395cdb8be9
luboslenco <lubos.lenco@gmail.com>
2025-07-16 23:01:12 +0200

    Tweak ssao

--- base/sources/ts/render_path_base.ts
+++ base/sources/ts/render_path_base.ts
@@ -255,8 +255,7 @@ function render_path_base_init_ssao() {
 
 function render_path_base_draw_ssao() {
 	let ssao: bool = config_raw.rp_ssao != false && context_raw.camera_type == camera_type_t.PERSPECTIVE;
-	// if (ssao && context_raw.ddirty > 0 && render_path_base_taa_frame > 0) {
-	if (ssao && render_path_base_taa_frame > 0) {
+	if (ssao && context_raw.ddirty > 0 && render_path_base_taa_frame > 0) {
 		if (map_get(render_path_render_targets, "singlea") == null) {
 			render_path_base_init_ssao();
 		}
