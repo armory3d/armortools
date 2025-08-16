2ce03047aafc8d3f01b99f0e8d6588e507f4ea35
luboslenco <lubos.lenco@gmail.com>
2025-08-16 20:14:19 +0200

    Use vulkan 1.3

--- base/project.js
+++ base/project.js
@@ -291,7 +291,7 @@ else if (platform === "android") {
 	project.target_options.android.package = flags.package;
 	project.target_options.android.permissions = ["android.permission.WRITE_EXTERNAL_STORAGE", "android.permission.READ_EXTERNAL_STORAGE", "android.permission.INTERNET"];
 	project.target_options.android.screenOrientation = ["sensorLandscape"];
-	project.target_options.android.minSdkVersion = 33;
+	project.target_options.android.minSdkVersion = 33; // android 13
 	project.target_options.android.targetSdkVersion = 36;
 	project.target_options.android.versionCode = 250601;
 	project.target_options.android.versionName = "1.0 alpha";
