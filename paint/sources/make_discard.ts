3dead5efa4c326be6b852e04656e110df714f86d
luboslenco <lubos.lenco@gmail.com>
2025-08-27 11:50:24 +0200

    Fix face fill mode

--- paint/sources/make_discard.ts
+++ paint/sources/make_discard.ts
@@ -15,8 +15,8 @@ function make_discard_face(kong: node_shader_t) {
 	node_shader_add_texture(kong, "textrianglemap", "_textrianglemap");
 	node_shader_add_constant(kong, "textrianglemap_size: float2", "_texpaint_size");
 	node_shader_add_constant(kong, "gbuffer_size: float2", "_gbuffer_size");
-	// node_shader_write_frag(kong, "var tex_coord_inp: float2 = gbuffer2[uint2(inp.x * constants.gbuffer_size.x, inp.y * constants.gbuffer_size.y)].ba;");
-	node_shader_write_frag(kong, "var tex_coord_inp4: float4 = gbuffer2[uint2(uint(inp.x * constants.gbuffer_size.x), uint(inp.y * constants.gbuffer_size.y))];");
+	// node_shader_write_frag(kong, "var tex_coord_inp: float2 = gbuffer2[uint2(constants.inp.x * constants.gbuffer_size.x, constants.inp.y * constants.gbuffer_size.y)].ba;");
+	node_shader_write_frag(kong, "var tex_coord_inp4: float4 = gbuffer2[uint2(uint(constants.inp.x * constants.gbuffer_size.x), uint(constants.inp.y * constants.gbuffer_size.y))];");
 	node_shader_write_frag(kong, "var tex_coord_inp: float2 = tex_coord_inp4.ba;");
 	node_shader_write_frag(kong, "var face_c1: float4 = textrianglemap[uint2(uint(tex_coord_inp.x * constants.textrianglemap_size.x), uint(tex_coord_inp.y * constants.textrianglemap_size.y))];");
 	node_shader_write_frag(kong, "var face_c2: float4 = sample_lod(textrianglemap, sampler_linear, input.tex_coord_pick, 0.0);");
