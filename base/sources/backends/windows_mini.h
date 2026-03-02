3a6e8da67ab32135b08e5c5ee0750d3fd366df7f
luboslenco <lubos.lenco@gmail.com>
2026-03-02 08:49:57 +0100

    base: fix compile

--- base/sources/backends/windows_mini.h
+++ base/sources/backends/windows_mini.h
@@ -68,31 +68,20 @@ typedef struct _OVERLAPPED {
 	HANDLE hEvent;
 } OVERLAPPED, *LPOVERLAPPED;
 
-WINBASEAPI BOOL WINAPI WriteConsoleA(HANDLE hConsoleOutput, CONST VOID *lpBuffer, DWORD nNumberOfCharsToWrite, LPDWORD lpNumberOfCharsWritten,
-                                     LPVOID lpReserved);
-
-WINBASEAPI BOOL WINAPI WriteConsoleW(HANDLE hConsoleOutput, CONST VOID *lpBuffer, DWORD nNumberOfCharsToWrite, LPDWORD lpNumberOfCharsWritten,
-                                     LPVOID lpReserved);
-
-WINBASEAPI VOID WINAPI OutputDebugStringA(LPCSTR lpOutputString);
-
-WINBASEAPI VOID WINAPI OutputDebugStringW(LPCWSTR lpOutputString);
-
+WINBASEAPI BOOL WINAPI   WriteConsoleA(HANDLE hConsoleOutput, CONST VOID *lpBuffer, DWORD nNumberOfCharsToWrite, LPDWORD lpNumberOfCharsWritten,
+                                       LPVOID lpReserved);
+WINBASEAPI BOOL WINAPI   WriteConsoleW(HANDLE hConsoleOutput, CONST VOID *lpBuffer, DWORD nNumberOfCharsToWrite, LPDWORD lpNumberOfCharsWritten,
+                                       LPVOID lpReserved);
+WINBASEAPI VOID WINAPI   OutputDebugStringA(LPCSTR lpOutputString);
+WINBASEAPI VOID WINAPI   OutputDebugStringW(LPCWSTR lpOutputString);
 WINBASEAPI HANDLE WINAPI GetStdHandle(DWORD nStdHandle);
-
-int WINAPI MultiByteToWideChar(UINT CodePage, DWORD dwFlags, LPCCH lpMultiByteStr, int cbMultiByte, LPWSTR lpWideCharStr, int cchWideChar);
-
+int WINAPI               MultiByteToWideChar(UINT CodePage, DWORD dwFlags, LPCCH lpMultiByteStr, int cbMultiByte, LPWSTR lpWideCharStr, int cchWideChar);
 WINBASEAPI HANDLE WINAPI CreateFileW(LPCWSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode, LPSECURITY_ATTRIBUTES lpSecurityAttributes,
                                      DWORD dwCreationDisposition, DWORD dwFlagsAndAttributes, HANDLE hTemplateFile);
-
-WINBASEAPI DWORD WINAPI GetFileSize(HANDLE hFile, LPDWORD lpFileSizeHigh);
-
-WINBASEAPI BOOL WINAPI ReadFile(HANDLE hFile, LPVOID lpBuffer, DWORD nNumberOfBytesToRead, LPDWORD lpNumberOfBytesRead, LPOVERLAPPED lpOverlapped);
-
-WINBASEAPI DWORD WINAPI SetFilePointer(HANDLE hFile, LONG lDistanceToMove, PLONG lpDistanceToMoveHigh, DWORD dwMoveMethod);
-
-WINBASEAPI BOOL WINAPI CloseHandle(HANDLE hObject);
-
-WINBASEAPI BOOL WINAPI WriteFile(HANDLE hFile, LPCVOID lpBuffer, DWORD nNumberOfBytesToWrite, LPDWORD lpNumberOfBytesWritten, LPOVERLAPPED lpOverlapped);
-
-int WINAPI MessageBoxW(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption, UINT uType);
+WINBASEAPI DWORD WINAPI  GetFileSize(HANDLE hFile, LPDWORD lpFileSizeHigh);
+WINBASEAPI DWORD WINAPI  GetFileAttributesA(LPCSTR lpFileName);
+WINBASEAPI BOOL WINAPI   ReadFile(HANDLE hFile, LPVOID lpBuffer, DWORD nNumberOfBytesToRead, LPDWORD lpNumberOfBytesRead, LPOVERLAPPED lpOverlapped);
+WINBASEAPI DWORD WINAPI  SetFilePointer(HANDLE hFile, LONG lDistanceToMove, PLONG lpDistanceToMoveHigh, DWORD dwMoveMethod);
+WINBASEAPI BOOL WINAPI   CloseHandle(HANDLE hObject);
+WINBASEAPI BOOL WINAPI   WriteFile(HANDLE hFile, LPCVOID lpBuffer, DWORD nNumberOfBytesToWrite, LPDWORD lpNumberOfBytesWritten, LPOVERLAPPED lpOverlapped);
+int WINAPI               MessageBoxW(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption, UINT uType);
