a77f3fbe1798f4402453acd798fc43d4e127aa1f
luboslenco <lubos.lenco@gmail.com>
2026-03-18 16:46:22 +0100

    base: cleanup

--- base/sources/iron_armpack.h
+++ base/sources/iron_armpack.h
@@ -89,11 +89,3 @@ int        armpack_map_get_i32(any_map_t *map, char *key);
         point_t *decoded = armpack_decode(b);
     }
 */
-
-// #ifdef __GNUC__
-// #define PACK(__Declaration__) __Declaration__ __attribute__((__packed__))
-// #endif
-
-// #ifdef _MSC_VER
-// #define PACK(__Declaration__) __pragma(pack(push, 1)) __Declaration__ __pragma(pack(pop))
-// #endif
